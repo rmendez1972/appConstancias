67dbb3a395ab26b115e2e5d4644112ec9947d1d3
Rafael Mendez Asencio <rmendez1972@hotmail.com>
2018-08-28 11:02:38 -0500

    refactorizando lesson 09

--- lessons/09-index-links/modules/App.js
+++ lessons/09-index-links/modules/App.js
@@ -5,8 +5,9 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <h1>React Router Tutorial</h1>
+        <h1>Usando React Router</h1>
         <ul role="nav">
+          <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
