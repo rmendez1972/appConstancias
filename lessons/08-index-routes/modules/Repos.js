cc78645a3ee95bc846d7c0fb1dcfa085be8bf7a6
Rafael Mendez Asencio <rmendez1972@hotmail.com>
2018-08-31 14:48:23 -0500

    setting a production web server with express

--- lessons/08-index-routes/modules/Repos.js
+++ lessons/08-index-routes/modules/Repos.js
@@ -5,7 +5,7 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <h2>Repos</h2>
+        <h2>Repos sub menu</h2>
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
